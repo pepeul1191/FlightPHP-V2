4cb954ed2ff0a127372fae34f4193d9a00811bcd
Pepe Valdivia <pepe.valdivia.caballero@gmail.com>
2024-06-08 07:21:47 -0500

    mejorando los errores get y post

--- app/Controllers/ErrorController.php
+++ app/Controllers/ErrorController.php
@@ -12,12 +12,6 @@ class ErrorController extends BaseController
   }
 
   public function not_found($number){
-    $method = Flight::request()->method;
-    if ($method === 'GET') {
-      Flight::view()->display('error.twig');
-    } else {
-      Flight::response()->status($number);
-      Flight::response()->write('Error 404: Recurso no encontrado');
-    }
+    Flight::view()->display('error.twig');
   }
 }
--- configs/routes.php
+++ configs/routes.php
@@ -9,7 +9,14 @@ Flight::route('/about', [HomeController::class, 'about']);
 Flight::route('GET /level/list', [HomeController::class, 'level_list']);
 Flight::route('GET /error/access/@number', [ErrorController::class, 'not_found']);
 // exceptions
-Flight::map('notFound', function () { Flight::redirect('/error/access/404'); });
+Flight::map('notFound', function () { 
+  $method = Flight::request()->method;
+  if ($method === 'GET') {
+    Flight::redirect('/error/access/404'); 
+  } else {
+    Flight::response()->write('Error 404: Recurso no encontrado');
+  }
+});
 
 // Puedes agregar más rutas según sea necesario
 Flight::route('/contact', function(){
